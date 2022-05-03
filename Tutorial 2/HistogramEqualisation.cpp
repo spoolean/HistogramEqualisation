e695dc7daedb601372f663f04b0efd3f4dc854e8
James Roche <jamieroche@outlook.com>
2022-05-03 14:15:51 +0100

    Fully working with enviroment variable changes

--- Tutorial 2/HistogramEqualisation.cpp
+++ Tutorial 2/HistogramEqualisation.cpp
@@ -32,7 +32,7 @@ int main(int argc, char **argv) {
 	//Part 1 - handle command line options such as device selection, verbosity, etc.
 	int platform_id = 0;
 	int device_id = 0;
-	string image_filename = "test_large.pgm";
+	string image_filename = "colour.ppm";
 
 	for (int i = 1; i < argc; i++) {
 		if ((strcmp(argv[i], "-p") == 0) && (i < (argc - 1))) { platform_id = atoi(argv[++i]); }
